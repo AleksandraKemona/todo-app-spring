1380aabed10dc60d99006be1bb61eb4bb6945a37
Aleksandra Kemona <aleksandra.kemona@gmail.com>
2023-05-11 22:10:21 +0200

    Adding: @GetMapping - taskById, @PutMapping, @PostMapping

--- src/main/java/io/akem/controler/TaskController.java
+++ src/main/java/io/akem/controler/TaskController.java
@@ -6,15 +6,13 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.net.URI;
 import java.util.List;
 
-// Adnotacja springowa, która dodaje kontroler oraz response body
+// Spring annotation, that adds controller and response body
 
 @RestController
 
@@ -36,8 +34,18 @@ class TaskController {
         logger.info("Custom pageable");
         return ResponseEntity.ok(repository.findAll(page).getContent());
     }
+    @GetMapping( path = "/tasks/{id}")
+    ResponseEntity<Task> readTask(@PathVariable int id){
+        if (!repository.existsById(id)){
+            return  ResponseEntity.notFound().build();
+        }
 
-    @GetMapping("/tasks/{id}")
+        return repository.findById(id).
+                map(task -> ResponseEntity.ok(task))
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @PutMapping("/tasks/{id}")
     ResponseEntity<?>updateTask(@PathVariable int id, @RequestBody @Valid Task toUpdate){
         if (!repository.existsById(id)){
             return  ResponseEntity.notFound().build();
@@ -47,5 +55,9 @@ class TaskController {
         return ResponseEntity.noContent().build();
     }
 
-
+    @PostMapping("/task")
+    ResponseEntity<Task> createTask (@RequestBody @Valid Task toCreate){
+        Task result = repository.save(toCreate);
+        return ResponseEntity.created(URI.create("/" + result.getId())).body(result);
+    }
 }
